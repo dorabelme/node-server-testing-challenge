a081ba2db4fcb36bb8f9ccc4c5ad3a61cb3aecae
Dora Belme <contact.dorabelme@gmail.com>
2019-09-19 23:09:31 -0400

    fixed tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,6 +3,8 @@ const request = require('supertest');
 const server = require('./server.js');
 const db = require('../database/dbConfig.js');
 
+const bcrypt = require('bcryptjs');
+
 
 describe('server', () => {
     beforeEach(async () => {
@@ -38,19 +40,39 @@ describe('server', () => {
     })
 });
 
-
+let token;
 
 describe('POST /LOGIN', () => {
+    it('Token should exist', async () => {
+        // await db.seed.run()
+        await db('users').insert([
+            { username: "admin", password: bcrypt.hashSync("admin", 16) },
+            { username: "testuser", password: bcrypt.hashSync("test", 16) }
+        ])
+
+        const res = await request(server).post('/auth/login')
+            .send({
+                username: "admin",
+                password: "admin"
+            })
+            .set('Content-Type', 'application/json')
+                
+        expect(res.status).toBe(200)
+        expect(res.body.token).toBeTruthy()
+
+        token = res.body.token;
+    });
+
     it('should return 200 status', () => {
         return request(server).post('/auth/login')
             .send({
-                username: "tommy",
-                password: "password2"
+                username: "admin",
+                password: "admin"
             })
             .set('Content-Type', 'application/json')
             .then(res => {
-                // expect(res.status).toBe(200)
-                expect(res.body.username).toBe('tommy')
+                expect(res.status).toBe(200)
+                expect(res.body.message).toBe('You are logged in')
             });
     })
 
@@ -63,12 +85,13 @@ describe('POST /LOGIN', () => {
             .set('Content-Type', 'application/json')
             .then(res => {
                 expect(res.status).toBe(200)
-                expect(res.body.message).toBe("You are logged in")
+                expect(res.body.message).toBe('You are logged in')
             })
     })
 })
 
 
+let hardcoded_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImRvcmEiLCJpYXQiOjE1Njg5MjI5MDEsImV4cCI6MTU2OTAwOTMwMX0.zuYasePVgpcJ6KgF8nLOAFV-ROlyoisTBB0yNGAGaZg';
 
 describe('GET /api/users', () => {
     it('returns json OK', () => {
@@ -78,7 +101,7 @@ describe('GET /api/users', () => {
 
     it('should return 200 Status', () => {
         return request(server).get('/api/users')
-            .set('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImRvcmEiLCJpYXQiOjE1Njg5MjI5MDEsImV4cCI6MTU2OTAwOTMwMX0.zuYasePVgpcJ6KgF8nLOAFV-ROlyoisTBB0yNGAGaZg')
+            .set('authorization', token)
             .then(res => {
                 expect(res.status).toBe(200)
             })
